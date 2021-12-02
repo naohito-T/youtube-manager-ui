69548716bf9759096d39a0da004bf7cd7037bb51
naohito-T <naohito.tanaka0523@gmail.com>
2021-12-02 12:52:34 +0900

    envの設定完了

--- youtube-manager-ui/nuxt.config.ts
+++ youtube-manager-ui/nuxt.config.ts
@@ -1,12 +1,16 @@
 import { NuxtConfig } from '@nuxt/types'
 
+const EXE_ENV = process.env.NODE_ENV ?? 'local'
+const ENV_FILE = `./env/decrypt/.env.${EXE_ENV}`
+require('dotenv').config({ path: ENV_FILE })
+
 const nuxtConfig: NuxtConfig = {
   // Global page headers: https://go.nuxtjs.dev/config-head
   srcDir: 'src/',
   head: {
     title: 'youtube-manager-ui',
     htmlAttrs: {
-      lang: 'en',
+      lang: 'ja',
     },
     meta: [
       { charset: 'utf-8' },
@@ -16,7 +20,9 @@ const nuxtConfig: NuxtConfig = {
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
-
+  env: {
+    YOUTUBE_API_KEY: process.env.YOUTUBE_API_KEY ?? 'none',
+  },
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
 
@@ -31,7 +37,6 @@ const nuxtConfig: NuxtConfig = {
     // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build',
     '@nuxtjs/composition-api/module',
-    '@nuxtjs/dotenv',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
@@ -39,6 +44,7 @@ const nuxtConfig: NuxtConfig = {
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
     '@nuxtjs/style-resources',
+    '@nuxtjs/dotenv',
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
